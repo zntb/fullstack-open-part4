fbcda841716aac5e72eb74682e2f3b11830d4192
zntb <zenetibi@yahoo.com>
2024-06-14 13:18:10 +0300

    Add finished exercise 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,11 +2,66 @@ const { test, describe } = require('node:test');
 const assert = require('node:assert');
 const listHelper = require('../utils/list_helper');
 
-describe('list_helper', () => {
-  test('dummy returns one', () => {
+describe('dummy', () => {
+  test('returns 1', () => {
     const blogs = [];
 
     const result = listHelper.dummy(blogs);
     assert.strictEqual(result, 1);
+  });
+});
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0,
+    },
+  ];
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+    assert.strictEqual(result, 27); // 5 + 12 + 10
   });
 });
